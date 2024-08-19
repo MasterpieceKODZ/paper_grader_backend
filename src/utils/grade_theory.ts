7390363eede67bc8611d3f5a93c211d40048f1a0
Gideon Faith <grail.masterpiece@gmail.com>
2024-08-19 09:16:27 +0100

    minor refactor

--- src/utils/grade_theory.ts
+++ src/utils/grade_theory.ts
@@ -3,7 +3,6 @@ import convertMapTypeToObjectLiteral from "./convert_map_to_object";
 import Candidate from "./types/Candidate";
 import getGPTPrompt from "./getGPTPrompt";
 
-
 const getGradeResult = (chatGptResponse: any) => {
 	if (chatGptResponse.choices.length == 0) {
 		return {
@@ -16,8 +15,7 @@ const getGradeResult = (chatGptResponse: any) => {
 		chatGptResponse.choices[0].message.content
 			.split("")
 			.filter(
-				(it: string, idx: number, arr: []) =>
-					idx > 6 && idx < arr.length - 4,
+				(it: string, idx: number, arr: []) => idx > 6 && idx < arr.length - 4,
 			)
 			.join(""),
 	);
@@ -40,32 +38,54 @@ async function gradeTheoryAnswers(
 		};
 	}
 
-	return candidateData.theory_answers.reduce(async (accPromise: any, studentAnswer: string, index: number) => {
-		const acc = await accPromise;
-		const courseDetails = ( convertMapTypeToObjectLiteral(course.theory_question_and_answer as any,) as any)[`${index + 1}`];
-		const totalScoreForQuestion = courseDetails.mark
-		const question = courseDetails.question
-		const context = courseDetails.context .map((psAnsIt: string) => `- ${psAnsIt}\n`)
-		.join( "\n\n--------------------------------------------------------------------------------------\n\n",);
-		const gradingRubric = courseDetails.rubric;
+	return candidateData.theory_answers.reduce(
+		async (accPromise: any, studentAnswer: string, index: number) => {
+			let acc = await accPromise;
+			const courseDetails = (
+				convertMapTypeToObjectLiteral(
+					course.theory_question_and_answer as any,
+				) as any
+			)[`${index + 1}`];
+			const totalScoreForQuestion = courseDetails.mark;
+			const question = courseDetails.question;
+			const context = courseDetails.context
+				.map((psAnsIt: string) => `- ${psAnsIt}\n`)
+				.join(
+					"\n\n--------------------------------------------------------------------------------------\n\n",
+				);
+			const gradingRubric = courseDetails.rubric;
 
-		const gptPrompt = getGPTPrompt(school_name,course.name,course.course_code,totalScoreForQuestion,question,studentAnswer,context,
-			gradingRubric,
-		);
-		
-		const openai = new OpenAI({ apiKey: `${process.env.OPEN_AI_KEY}` });
-		const completion: any = await openai.chat.completions.create({messages: [{ role: "user", content: gptPrompt }],model: "gpt-4o-mini",
-			temperature: 0.5,
-		});
+			const gptPrompt = getGPTPrompt(
+				school_name,
+				course.name,
+				course.course_code,
+				totalScoreForQuestion,
+				question,
+				studentAnswer,
+				context,
+				gradingRubric,
+			);
 
-		const gradeResult = getGradeResult(completion);
-		acc.theoryScore += gradeResult!.totalScore;
-		acc.theoryGradeAndSummary.push({question: question,answer: studentAnswer,mark: gradeResult!.totalScore,reason: gradeResult!.summary,});
-		return acc;
+			const openai = new OpenAI({ apiKey: `${process.env.OPEN_AI_KEY}` });
+			const completion: any = await openai.chat.completions.create({
+				messages: [{ role: "user", content: gptPrompt }],
+				model: "gpt-4o-mini",
+				temperature: 0.5,
+			});
 
-	}, Promise.resolve({ theoryScore: 0, theoryGradeAndSummary: [
-		{ question: '', answer: '', mark: '', reason: '' },
-	] }));
+			const gradeResult = getGradeResult(completion);
+			acc.theoryScore += gradeResult!.totalScore;
+
+			acc.theoryGradeAndSummary.push({
+				question: question,
+				answer: studentAnswer,
+				mark: gradeResult!.totalScore,
+				reason: gradeResult!.summary,
+			});
+			return acc;
+		},
+		Promise.resolve({ theoryScore: 0, theoryGradeAndSummary: [] }),
+	);
 }
 
 export default gradeTheoryAnswers;
